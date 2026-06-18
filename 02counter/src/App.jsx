8305932001694854f6971f509dadf37b14a10464
shreyasrivastava0101 <shreyasrivastava8078@gmail.com>
2026-06-19 01:55:43 +0530

    increase and decrease counter using hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import React,{ useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(0)
+
+   const addValue=() =>{
+     if(count<20){
+      setCount(count+1)
+     }
+   }
+
+   const removeValue=()=>{
+    if(count>0){
+      setCount(count-1)
+    }
+   }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value:{count}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>remove value</button>
+    </>
+  )
+}
+
+export default App
