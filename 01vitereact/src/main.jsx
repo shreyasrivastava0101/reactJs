95f9fa1b61423117d718f7e13d5d70cf9b254166
shreyasrivastava0101 <shreyasrivastava8078@gmail.com>
2026-06-19 01:01:46 +0530

    custon react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,16 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  <App />
-  </StrictMode>,
+const reactElement=React.createElement(
+  'a',
+  {href:'https:/google.com',target:'_blank'},
+  'click ne to open google'
 )
- 
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+  // <App />
+  // </StrictMode>,
+  reactElement
+)
